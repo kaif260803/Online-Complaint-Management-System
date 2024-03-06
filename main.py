7de4688301369c0aee9e87834207103e13b19a74
kaif260803 <mk8288@srmist.edu.in>
2024-03-06 20:03:05 +0530

    added the title of the application

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from create_tables import create_tables
 def main():
     create_tables()
     root = tk.Tk()
+    root.title("Online Complaint Management System")
     conn = sqlite3.connect("online_management_system.db")
     gui_utils = GUIUtils(root, conn)
     root.mainloop()
